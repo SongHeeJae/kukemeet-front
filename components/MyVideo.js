2c8ac54bf0b89f9c958142084e67c3ee10fef7c4
SongHeeJae <gmlwo308@gmail.com>
2021-03-07 19:30:09 +0900

    fix: MyVideo도 메인 비디오 변환 가능

--- components/MyVideo.js
+++ components/MyVideo.js
@@ -1,6 +1,8 @@
-import React, { useRef, useEffect, memo } from "react";
+import React, { useRef, useEffect, memo, useCallback } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { changeMainStreamSuccess } from "../reducers/videoroom";
+import { useDispatch } from "react-redux";
 
 const VideoWrapper = styled.video`
   width: 100%;
@@ -8,19 +10,31 @@ const VideoWrapper = styled.video`
 `;
 
 const MyVideo = () => {
+  const dispatch = useDispatch();
   const videoRef = useRef();
   const { myFeed } = useSelector((state) => state.videoroom);
+  const { stream } = myFeed;
   useEffect(() => {
-    if (!myFeed.stream) return;
-    videoRef.current.srcObject = myFeed.stream;
-  }, [myFeed.stream]);
+    if (!stream) return;
+    videoRef.current.srcObject = stream;
+  }, [stream]);
+
+  const onClick = useCallback(() => {
+    dispatch(changeMainStreamSuccess({ stream, display: "나" }));
+  }, [stream]);
 
   if (!myFeed.stream) {
     <div>로딩중..</div>;
   }
   return (
     <>
-      <VideoWrapper ref={videoRef} autoPlay playsInline muted />
+      <VideoWrapper
+        ref={videoRef}
+        autoPlay
+        playsInline
+        muted
+        onClick={onClick}
+      />
     </>
   );
 };
