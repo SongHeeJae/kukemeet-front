e79d3f9d8afd659ee384f9de192de2a71a10ee98
SongHeeJae <gmlwo308@gmail.com>
2021-03-11 03:52:42 +0900

    fix: server url 수정

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ export const mediaServerUrl =
     : ["http://media.kukemeet.com/janus"];
 export const backServerUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.kukemeet.com/back"
+    ? "https://api.kukemeet.com"
     : "http://localhost:8080/";
 export const fileWebSocketUrl =
   process.env.NODE_ENV === "production"
